fc76866655c1f16c26ad0a1a2eb5dfb720f1d14a
Raymond Loman <raymond@carpago.nl>
2019-04-19 09:29:27 +0200

    Add demo for date time. And add some demo code for the case BlackJack

--- src/nl/workingspirit/schilderen/datetime/AdvancedApp.java
+++ src/nl/workingspirit/schilderen/datetime/AdvancedApp.java
@@ -3,31 +3,42 @@ package nl.workingspirit.schilderen.datetime;
 import java.time.*;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeFormatterBuilder;
+import java.time.temporal.ChronoField;
+import java.time.temporal.TemporalField;
 
 // (c) https://www.logicbig.com/how-to/code-snippets/jcode-java-8-date-time-api-localdatetime-from.html
 public class AdvancedApp {
 
     public static void main(String[] args) {
 
-        parse();
+       get();
 
     }
 
-    public static void parse() {
+    public static void get() {
+        LocalDateTime myBirthDate = LocalDateTime.of(1968,8,9,15,30,0);
 
-        //parse
-        //ISO8601
-        LocalDateTime localDateTime = LocalDateTime.parse("2019-04-19T19:23:00");
+//       myBirthDate = LocalDateTime.now().minusMonths(1);
 
-        System.out.println(localDateTime);
+        int hour = myBirthDate.get(ChronoField.CLOCK_HOUR_OF_AMPM);
 
+        System.out.println(hour);
+        System.out.println(myBirthDate.get(ChronoField.ALIGNED_DAY_OF_WEEK_IN_MONTH)); // => 2 since it is the second day in the week starting on 1-08-1968 and that was a Thursday
+
+        int dayOfWeekInYear = myBirthDate.get(ChronoField.ALIGNED_DAY_OF_WEEK_IN_YEAR);
+        System.out.println("Day of week in year: "+dayOfWeekInYear);
+
+    }
+
+    public static void format() {
+
+        LocalDateTime localDateTime = LocalDateTime.now();
         DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ISO_DATE_TIME;
         String formattedString = localDateTime.format(dateTimeFormatter);
         System.out.println(formattedString);
 
-
         DateTimeFormatterBuilder builder = new  DateTimeFormatterBuilder();
-        builder.appendPattern("dd-MM-yyyy");
+        builder.appendPattern("dd-MMMM-yyyy");
 
         dateTimeFormatter = builder.toFormatter();
 
@@ -43,7 +54,15 @@ public class AdvancedApp {
         dateTimeFormatter = builder.toFormatter();
         formattedString = localDateTime.format(dateTimeFormatter);
         System.out.println(formattedString);
+    }
 
+    public static void parse() {
+
+        //parse
+        //ISO8601
+        LocalDateTime localDateTime = LocalDateTime.parse("2019-04-19T19:23:00");
+
+        System.out.println(localDateTime);
     }
 
     public static void from() {
